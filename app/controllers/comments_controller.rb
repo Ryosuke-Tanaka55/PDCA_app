cc5d726deeb2079850511013e264b7b6fef1a48b
Ryousuke-Tanaka <tanaka.r0815@gmail.com>
2021-07-13 23:45:46 +0900

    comment修正_20210713

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,21 +7,30 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     #投稿に紐づいたコメントを作成
     @comment = @post.comments.build(comment_params)
-    @comment.user_id = current_user.id
-    @comment.save
-    # 通知機能
-    @post.create_notification_comment!(current_user, @comment.id)
-    render :index
+    if @comment.save
+      # 通知機能
+      @post.create_notification_comment!(current_user, @comment.id)
+      flash[:success] = "コメント投稿に成功しました。"
+    else
+      flash[:danger] = "コメント投稿に失敗しました。"
+    end
+    redirect_to user_post_url(@user.id, @post.id)
   end
+
 
   def destroy
     @comment = Comment.find(params[:id])
-    @comment.destroy
-    render :index
+    if @comment.destroy
+      @post = Post.find(params[:post_id])
+      flash[:success] = "コメント削除に成功しました。"
+    else
+      flash[:danger] = "コメント削除に失敗しました。"
+    end
+      redirect_to user_post_url(@user.id, @post.id)
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:comment, :post_id, :user_id)
+    params.require(:comment).permit(:comment, :post_id).merge(user_id: current_user.id)
   end
 end
